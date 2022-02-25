e032b582b6e7b262ce76d5b661a47d68a129ec53
Scott Everett <burningcupidxxx@gmail.com>
2022-02-25 08:58:55 -0800

    add removeFriends function to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -73,6 +73,21 @@ module.exports = {
             .catch((err) => res.status(500).json(err));
     },
 
+    // Remove friend from user
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friend: { friendId: req.params.friendId } } },
+            { runValidators: true, new: true },
+        )
+            .then((user) => 
+            !user
+                ? res.status(404).json({ message: 'No user found with that ID' })
+                : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
 
 
 };
